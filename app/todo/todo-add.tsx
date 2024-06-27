104d39160cf2359ab551874561dca623aca21253
Rahma <rahma2029@hotmail.com>
2024-06-27 10:24:52 +1000

    change submit button color into green

--- app/todo/todo-add.tsx
+++ app/todo/todo-add.tsx
@@ -14,7 +14,7 @@ export default function AddTodo() {
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="todo-title" type="text" placeholder="Title" />
                 </div>
                 <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={add}>
+                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={add}>
                         Submit
                     </button>
                 </div>
